a01fe4a256bb71cd6f20fbfddc6a157b46a4108e
Todd Williams <toddlessness@gmail.com>
2021-06-19 21:22:21 -0700

    refactor

--- src/modules/InStock.tsx
+++ src/modules/InStock.tsx
@@ -10,22 +10,43 @@ export function InStock({onStockChange}: Props) {
     const stock = React.useContext(CurrentStock)
     const [symbol, setSymbol] = React.useState('+')
 
-    function toggleAccordion() {
-        // @ts-ignore
-        ;[...document.getElementsByClassName('stock')].forEach(
-            toggleItemDisplay
-        )
-        toggleSymbol()
-    }
-
     function toggleSymbol() {
         symbol === '+' ? setSymbol('–') : setSymbol('+')
+    }
+
+    function toggleAccordion() {
+        const stockList = document.getElementsByClassName('stock')
+        // @ts-ignore
+        ;[...stockList].forEach(toggleItemDisplay)
+        toggleSymbol()
     }
 
     function toggleItemDisplay(item: HTMLElement) {
         item.style.display === 'block'
             ? (item.style.display = 'none')
             : (item.style.display = 'block')
+    }
+
+    function listStockIngredients() {
+        return [...Object.keys(allIngredients)].sort().map(cat => (
+            <ul key={cat} className="stock">
+                ~{cat}~
+                {[...Object.keys(allIngredients[cat])].sort().map(ing => {
+                    const ingredient = allIngredients[cat][ing]
+                    return (
+                        <li key={ingredient} className="stock">
+                            <input
+                                id={ingredient}
+                                type="checkbox"
+                                onChange={onStockChange}
+                                checked={stock.includes(ingredient)}
+                            />
+                            <label htmlFor={ingredient}>{ingredient}</label>
+                        </li>
+                    )
+                })}
+            </ul>
+        ))
     }
 
     return (
@@ -35,28 +56,7 @@ export function InStock({onStockChange}: Props) {
                     Current stock <em>({symbol})</em>
                 </strong>
             </p>
-            {[...Object.keys(allIngredients)].sort().map(c => (
-                <ul key={c} className="stock">
-                    ~{c}~
-                    {[...Object.keys(allIngredients[c])].sort().map(i => {
-                        return (
-                            <li key={allIngredients[c][i]} className="stock">
-                                <input
-                                    id={allIngredients[c][i]}
-                                    type="checkbox"
-                                    onChange={onStockChange}
-                                    checked={stock.includes(
-                                        allIngredients[c][i]
-                                    )}
-                                />
-                                <label htmlFor={allIngredients[c][i]}>
-                                    {allIngredients[c][i]}
-                                </label>
-                            </li>
-                        )
-                    })}
-                </ul>
-            ))}
+            {listStockIngredients()}
             <p>With what you have in stock...</p>
         </form>
     )
