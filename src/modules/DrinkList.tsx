ad22eb8d006857a8dc4eefee65faf99c50142c17
Todd Williams <toddlessness@gmail.com>
2021-06-19 20:58:19 -0700

    refactor

--- src/modules/DrinkList.tsx
+++ src/modules/DrinkList.tsx
@@ -4,19 +4,13 @@ import {AvailableDrinks} from '../App'
 
 export function DrinkList() {
     const drinks = React.useContext(AvailableDrinks) ?? []
-
-    function NumDrinks() {
-        const numDrinks = drinks.length
-        return (
-            <p>
-                {numDrinks} {numDrinks === 1 ? 'drink' : 'drinks'} found
-            </p>
-        )
-    }
+    const numDrinks = drinks.length
 
     return (
         <div id="drink-list">
-            <NumDrinks />
+            <p>
+                {numDrinks} {numDrinks === 1 ? 'drink' : 'drinks'} found
+            </p>
             {drinks.map(drink => (
                 <Drink key={drink.name} drink={drink} />
             ))}
