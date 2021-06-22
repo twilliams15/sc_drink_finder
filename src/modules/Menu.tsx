50b6b977afc26e6b4213509e1b75b37ae1c4a5f8
Todd Williams <toddlessness@gmail.com>
2021-06-21 20:26:55 -0700

    menu prep

--- src/modules/Menu.tsx
+++ src/modules/Menu.tsx
@@ -5,10 +5,36 @@ import {getAvailableDrinks} from '../support/helpers'
 export function Menu() {
     const [stock] = React.useContext(CurrentStock)
     const [, setDisplayedDrinks] = React.useContext(AvailableDrinks)
+    const availableDrinks = getAvailableDrinks(stock)
 
     React.useEffect(() => {
-        setDisplayedDrinks(getAvailableDrinks(stock))
+        setDisplayedDrinks(availableDrinks)
     }, [])
 
-    return <div>Hello World</div>
+    function Drink({name}: {name: string}) {
+        return <h2>{name}</h2>
+    }
+
+    function Ingredients({items}: {items: string[]}) {
+        const removeParens = (item: string) => item.substr(0, item.indexOf('('))
+
+        return (
+            <ul>
+                {items.map(item => (
+                    <li>{item.includes('(') ? removeParens(item) : item}</li>
+                ))}
+            </ul>
+        )
+    }
+
+    return (
+        <div>
+            {availableDrinks.map(drink => (
+                <>
+                    <Drink name={drink.name} />
+                    <Ingredients items={drink.ingredients} />
+                </>
+            ))}
+        </div>
+    )
 }
